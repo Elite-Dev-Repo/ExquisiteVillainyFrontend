a99f52ad796d5264d4e2e4c80027af4bfcce2e5f
Elite Dev <oyenekantomiwa2.com>
2026-03-19 00:00:45 +0000

    feat: initial commit

---
+++ vite.config.js
@@ -0,0 +1,13 @@
+import path from "path";
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      // This maps the "@" symbol to the "src" directory
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+});
